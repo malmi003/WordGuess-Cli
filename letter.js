15edf713453bd8ebbc321c07d32567ba19123fab
Amanda Malmin <amandamalmin@gmail.com>
2018-07-15 10:01:09 -0500

    letter.js working

--- letter.js
+++ letter.js
@@ -1,9 +1,33 @@
-// -- ----Letter.js----: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+// ----Letter.js----: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
 
-//   -- A string value to store the underlying character for the letter
+let Letter = function (char) {
+    // A string value to store the underlying character for the letter
+    this.char = char;
 
-//   -- A boolean value that stores whether that letter has been guessed yet
+    // A boolean value that stores whether that letter has been guessed yet
+    this.hasBeenGuessedBool = false;
 
-//   -- A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+    // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+    this.returnChar = function () {
+        if (this.hasBeenGuessedBool) {
+            return char;
+        } else return "_ "
+    }
 
-//   -- A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+    // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+    this.compareLetters = function (guessedChar) {
+        if (guessedChar == this.char) {
+            this.hasBeenGuessedBool = true;
+        }
+
+        //may change this line so functions remain separate...
+        console.log(this.returnChar());
+    }
+}
+
+module.exports = Letter;
+
+// let a = new Letter ("a")
+// console.log(a)
+
+// a.compareLetters(process.argv[2]);
--- word.js
+++ word.js
@@ -1,7 +1,11 @@
-// -- ----Word.js----: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+// ----Word.js----: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+let Letter = require("./letter");
 
-//   -- An array of `new` Letter objects representing the letters of the underlying word
+// An array of `new` Letter objects representing the letters of the underlying word
+let wordToGuess = "Hubble";
 
-//   -- A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
 
-//   -- A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+// A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+
+module.exports = Word;
